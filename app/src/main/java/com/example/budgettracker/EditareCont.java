20cbce9aa3cd4f2e37991392885b2ebcfa785018
mihaiLambru <mihailambru99@gmail.com>
2020-11-08 19:06:35 +0200

    Am adaugat o activitate si pentru editarea contului

---
+++ app/src/main/java/com/example/budgettracker/EditareCont.java
@@ -0,0 +1,14 @@
+package com.example.budgettracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditareCont extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editare_cont);
+    }
+}

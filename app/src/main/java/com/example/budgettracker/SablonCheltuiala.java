e8d40c7b69b384cf7fe0c5a48decf5bec4ad1fde
mihaiLambru <mihailambru99@gmail.com>
2020-11-17 16:47:14 +0200

    Am adaugat activitatea SablonCheltuiala

---
+++ app/src/main/java/com/example/budgettracker/SablonCheltuiala.java
@@ -0,0 +1,14 @@
+package com.example.budgettracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SablonCheltuiala extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sablon_cheltuiala);
+    }
+}
